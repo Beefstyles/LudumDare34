85343df1d0e460c7c89d87e3f43105091cf4d14f
Beefstyles <glenwillis@gmail.com>
2015-12-13 21:48:03 +0800

    Added main screen with description & player select.

---
+++ Assets/Scripts/ButtonControlScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonControlScript : MonoBehaviour {
+
+
+    public void OnePlayerSelected()
+    {
+        CarryOverInfoScript.player1AI = false;
+        CarryOverInfoScript.player2AI = true;
+        CarryOverInfoScript.player3AI = true;
+        CarryOverInfoScript.player4AI = true;
+    }
+
+    public void TwoPlayersSelected()
+    {
+        CarryOverInfoScript.player1AI = false;
+        CarryOverInfoScript.player2AI = false;
+        CarryOverInfoScript.player3AI = true;
+        CarryOverInfoScript.player4AI = true;
+    }
+
+    public void ThreePlayersSelected()
+    {
+        CarryOverInfoScript.player1AI = false;
+        CarryOverInfoScript.player2AI = false;
+        CarryOverInfoScript.player3AI = false;
+        CarryOverInfoScript.player4AI = true;
+    }
+
+    public void FourPlayersSelected()
+    {
+        CarryOverInfoScript.player1AI = false;
+        CarryOverInfoScript.player2AI = false;
+        CarryOverInfoScript.player3AI = false;
+        CarryOverInfoScript.player4AI = false;
+    }
+}
